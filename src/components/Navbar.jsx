86fc668a581fad1deef7d355fcedd7635691fed4
farxodnazarov <farhodnazarov002gmail.com>
2024-07-01 11:55:53 +0500

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,31 +2,71 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>
-      <nav>
-        <div className="navbar bg-base-100">
-          <div className="flex-1">
-            <a className="btn btn-ghost text-2xl">daisyUI</a>
+    <div className="navbar bg-base-100  px-5 rounded-2xl ">
+      <div className="navbar-start ">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
-          <div className="flex-none">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
 
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-              <li className="px-5 bg-red-500 text-white font-medium rounded-2xl hover:bg-black">
-                <Link to="/login">Logout</Link>
-              </li>
-            </ul>
-          </div>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            {/* <li>
+              <a>Item 3</a>
+            </li> */}
+          </ul>
         </div>
-      </nav>
+        <Link to="/" className="btn btn-ghost text-xl">
+          daisyUI
+        </Link>
+      </div>
+      <div className="navbar-center hidden rounded-2xl lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+
+          {/* <li>
+            <a>Item 3</a>
+          </li> */}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <Link to="/login" className="btn">
+          Logout
+        </Link>
+      </div>
     </div>
   );
 }
